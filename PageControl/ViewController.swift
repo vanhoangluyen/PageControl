4ce511d6d34abe922b3cb1ef37f44129aec95ede
HoangLuyen <vanhoangluyen@gmail.com>
2018-08-02 08:18:04 +0700

    finish add pageControl

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        pageControl.currentPage = 0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = scrollView.frame.width
+        let currentPage: CGFloat = floor((scrollView.contentOffset.x - pageNumber/2)/pageNumber)+1
+        pageControl.currentPage = Int(currentPage)
+    }
 
 }
 
